82a5228c8ff8ebe63180ea4976d5d42b34879348
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-01-22 16:46:38 +0100

    Add pages ideas and some starting styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./styles/application.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>This is test</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
+        perferendis consequatur vitae reprehenderit maiores ipsam? Omnis fugit
+        minus facilis qui ea quisquam dolorem totam quam, pariatur enim, dolores
+        eligendi veniam?
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
