36f135972637f6e8b5bb0da2e300ac5c9e6d4a51
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-01-22 17:34:33 +0100

    Add NotFound text

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import "../styles/pageStyles/NotFound.css";
+
+// fix this
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="mainContainer">
+      <div className="notFoundImageBox">
+        <p></p>
+      </div>
+      <div className="notFoundText">
+        <p style={{ fontWeight: "bold" }}>
+          - 404 - NOT FOUND! <br />
+          <span style={{ fontWeight: "normal" }}>
+            Unfortunately, this page does not exists.
+          </span>
+        </p>
+        <p style={{ fontSize: "30px" }}></p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
