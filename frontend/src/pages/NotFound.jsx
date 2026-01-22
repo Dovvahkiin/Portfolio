4051e1f5e51cf84213e33f124f45556166627337
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-01-22 22:17:01 +0100

    NotFound fixed

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,23 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/pageStyles/NotFound.css";
-
+import warning from "../assets/images/icons/warning.png";
 // fix this
 
 const NotFound = () => {
   return (
-    <div className="mainContainer">
-      <div className="notFoundImageBox">
-        <p></p>
+    <>
+      <div className="mainContainer">
+        <div className="notFoundImageBox">
+          <img src={warning} alt="" srcset="" />
+        </div>
+        <div className="notFoundText">
+          <p style={{ fontWeight: "bold" }}>
+            - 404 - NOT FOUND! <br />
+            <span style={{ fontWeight: "normal" }}>
+              Unfortunately, this page does not exists.
+            </span>
+          </p>
+          <p style={{ fontSize: "30px" }}></p>
+        </div>
       </div>
-      <div className="notFoundText">
-        <p style={{ fontWeight: "bold" }}>
-          - 404 - NOT FOUND! <br />
-          <span style={{ fontWeight: "normal" }}>
-            Unfortunately, this page does not exists.
-          </span>
-        </p>
-        <p style={{ fontSize: "30px" }}></p>
-      </div>
-    </div>
+
+      <Link to={"/"} className="returnButton">
+        <div className="buttonBox">Return to Homepage</div>
+      </Link>
+    </>
   );
 };
 
